9240c44391ae306fd321c2d9f319a0fee2c4ccb9
westonplatter <westonplatter@gmail.com>
2020-06-28 10:00:27 -0600

    add email

--- main.py
+++ main.py
@@ -8,23 +8,45 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t','--token',required=True,help="The GitHub Token.")
-parser.add_argument('-r','--repo',required=True,help="The GitHub Repo,in the form like 'user/repo'.")
+parser.add_argument("-t", "--token", required=True, help="The GitHub Token.")
+parser.add_argument(
+    "-r", "--repo", required=True, help="The GitHub Repo,in the form like 'user/repo'."
+)
 args = parser.parse_args()
 
-owner = args.repo.split('/')[0]
-repo = args.repo.split('/')[1]
+owner = args.repo.split("/")[0]
+repo = args.repo.split("/")[1]
 
-headers = {"Authorization": "token "+args.token}
+headers = {"Authorization": "token " + args.token}
 
-fields = ["username","name","blog", "company", "bio","avatar_url","hireable" , "num_followers", "num_following","created_at","star_time"]
+fields = [
+    "username",
+    "name",
+    "email",
+    "blog",
+    "company",
+    "bio",
+    "avatar_url",
+    "hireable",
+    "num_followers",
+    "num_following",
+    "created_at",
+    "star_time",
+]
+
 
 def run_query(query):
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    request = requests.post(
+        "https://api.github.com/graphql", json={"query": query}, headers=headers
+    )
     if request.status_code == 200:
         return request.json()
     else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+        raise Exception(
+            "Query failed to run by returning code of {}. {}".format(
+                request.status_code, query
+            )
+        )
 
 
 query = """
@@ -42,6 +64,7 @@ query {{
         node {{
           login
           name
+          email
 	  bio
           company
           isHireable
@@ -63,43 +86,67 @@ query {{
 
 star_list = []
 hasNextPage = True
-endCursor = "" # Start from begining
+endCursor = ""  # Start from begining
 count = 0
-with open('stargazers.csv', 'w') as stars:
+with open("stargazers.csv", "w") as stars:
     stars_writer = csv.writer(stars)
     stars_writer.writerow(fields)
     while hasNextPage:
-        this_query = query.format(owner,repo,endCursor)
-        result = run_query(this_query) # Execute the query
-        #print(this_query)
-        #print(result)
-        hasNextPage = result['data']['repository']['stargazers']['pageInfo']['hasNextPage']
-        endCursor = result['data']['repository']['stargazers']['pageInfo']['endCursor']
+        this_query = query.format(owner, repo, endCursor)
+        result = run_query(this_query)  # Execute the query
+        # print(this_query)
+        # print(result)
+        hasNextPage = result["data"]["repository"]["stargazers"]["pageInfo"][
+            "hasNextPage"
+        ]
+        endCursor = result["data"]["repository"]["stargazers"]["pageInfo"]["endCursor"]
         endCursor = ', after: "' + endCursor + '"'
-        data = result['data']['repository']['stargazers']['edges']
+        data = result["data"]["repository"]["stargazers"]["edges"]
 
         for item in data:
-            username = item['node']['login']
-            name = item['node']['name']
-            num_followers = item['node']['followers']['totalCount']
-            num_following = item['node']['following']['totalCount']
-            hireable = item['node']['isHireable']
-            company = item['node']['company']
-            bio = item['node']['bio']
-            avatar_url = item['node']['avatarUrl']
-            blog = item['node']['websiteUrl']
+            username = item["node"]["login"]
+            name = item["node"]["name"]
 
-            created_at = item['node']['createdAt']
-            created_at = datetime.datetime.strptime(created_at,'%Y-%m-%dT%H:%M:%SZ')
-            created_at = created_at + datetime.timedelta(hours=-5) # EST
-            created_at = created_at.strftime('%Y-%m-%d %H:%M:%S')
+            if "email" in item["node"]:
+                email = item["node"]["email"]
+            else:
+                email = ""
 
-            star_time = datetime.datetime.strptime(item['starredAt'],'%Y-%m-%dT%H:%M:%SZ')
-            star_time = star_time + datetime.timedelta(hours=-5) # EST
-            star_time = star_time.strftime('%Y-%m-%d %H:%M:%S')
-            star_list.append((username,star_time))
-            stars_writer.writerow([username,name,blog,company,bio,avatar_url,hireable,num_followers,num_following,created_at,star_time])
+            num_followers = item["node"]["followers"]["totalCount"]
+            num_following = item["node"]["following"]["totalCount"]
+            hireable = item["node"]["isHireable"]
+            company = item["node"]["company"]
+            bio = item["node"]["bio"]
+            avatar_url = item["node"]["avatarUrl"]
+            blog = item["node"]["websiteUrl"]
+
+            created_at = item["node"]["createdAt"]
+            created_at = datetime.datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+            created_at = created_at + datetime.timedelta(hours=-5)  # EST
+            created_at = created_at.strftime("%Y-%m-%d %H:%M:%S")
+
+            star_time = datetime.datetime.strptime(
+                item["starredAt"], "%Y-%m-%dT%H:%M:%SZ"
+            )
+            star_time = star_time + datetime.timedelta(hours=-5)  # EST
+            star_time = star_time.strftime("%Y-%m-%d %H:%M:%S")
+            star_list.append((username, star_time))
+            stars_writer.writerow(
+                [
+                    username,
+                    name,
+                    email,
+                    blog,
+                    company,
+                    bio,
+                    avatar_url,
+                    hireable,
+                    num_followers,
+                    num_following,
+                    created_at,
+                    star_time,
+                ]
+            )
 
         count = count + 100
         print(str(count) + "users processed.")
-
